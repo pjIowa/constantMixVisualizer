19a2730943c6984724aa6a4cbd7bc03b5e17b30a
Prajwal Kedilaya <pjkedilaya@gmail.com>
2020-06-27 21:32:02 -0700

    load data and initial variables

--- trading_strats.py
+++ trading_strats.py
@@ -1,3 +1,16 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+
+# Adjusted close downloaded from June 25, 2000 to June 26, 2020 inclusive of 3 stocks using data from Yahoo Finance
+MSFT=pd.read_csv('MSFT.csv')["Adj Close"].to_numpy()
+CSCO=pd.read_csv('CSCO.csv')["Adj Close"].to_numpy()
+GE=pd.read_csv('GE.csv')["Adj Close"].to_numpy()
+prices = np.vstack((MSFT, CSCO, GE))
+
+N = len(prices[0])
+initial_wealth = 10**6
+cost_per_unit = 0.005
+num_stocks = len(prices)
+num_assets = num_stocks+1.
+
